a7c9901d8f389975918a9ed1d70a92a5bb6d7b0a
GBondarek <georgettebondarek@gmail.com>
2020-11-05 11:56:10 -0500

    added variables and clock function

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // identify hours of work 9AM-5PM
-// 1 hour increments for scheduling on hour
 // create variables 
+var workHours = []; 
+var availableHours = {};
+// moment().year(year).month(month).date(day)
+var day = moment();
+var currentTime = m.hour();
+
+// Need clock to track current day time
+function clock() { 
+    var dateString = moment().format('MMMM Do YYYY, h:mm:ss a');
+     $('#currentDay').html(dateString);
+}
+
+
+
+// 1 hour increments for scheduling on hour
 
 // use moment JS library to find code 
 // how to save to local storage
