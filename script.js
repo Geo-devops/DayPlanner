dddaedc9c2d338d26cc34ae55dabf3a4519617ff
GBondarek <georgettebondarek@gmail.com>
2020-11-06 16:06:34 -0500

    added hour blocks

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-// identify hours of work 9AM-5PM
-// create variables 
-var workHours = []; 
-var availableHours = {};
-// moment().year(year).month(month).date(day)
-var day = moment();
-var currentTime = m.hour();
+// // identify hours of work 9AM-5PM
+// // create variables 
+// var workHours = []; 
+// var availableHours = {};
+// // moment().year(year).month(month).date(day)
+// var day = moment();
+// var currentTime = m.hour();
 
-// Need clock to track current day time
-function clock() { 
-    var dateString = moment().format('MMMM Do YYYY, h:mm:ss a');
-     $('#currentDay').html(dateString);
-}
+// // Need clock to track current day time
+// function clock() { 
+//     var dateString = moment().format('MMMM Do YYYY, h:mm:ss a');
+//      $('#currentDay').html(dateString);
+// }
 
-// clock times and areas to schedule tasks
+// // clock times and areas to schedule tasks
 
-for (var hour = 9; hour < 18;) {
-    workHours.push(moment({hour}).format());
-    // need to append
-    $('.container').append()
+// for (var hour = 9; hour < 18;) {
+//     workHours.push(moment({hour}).format());
+//     // need to append
+//     $('.container').append('<div class='row time-block' data-time='${hour}'>
+//     <div class= 'col-md hour'>
+//         <p>${moment({hour}).format('h a')}</p>
+//     );
+// }
 
-}
+// // 1 hour increments for scheduling on hour
+// $('time-block').each(function() {
+// $(this).find('.text-area').val(availableHours[$(this).attr('date-time')].value);
+// });
+// // how to save to local storage
+$(document).ready(function(){
 
-// 1 hour increments for scheduling on hour
-$('time-block').each(function() {
-$(this).find('.text-area').val(availableHours)
+$(".saveBtn").on("click", function(){
+    var activity = $(this).siblings(".description").val().trim()
+    var hour = $(this).parent().attr("id")
+    console.log(hour)
+    console.log(activity)
+    console.log(localStorage)
 
-
-}
-// how to save to local storage
+    localStorage.setItem(hour, activity)
+    
+})
