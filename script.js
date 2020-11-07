027f327c45bf06aa08111b0d3852b037c0d8b537
GBondarek <georgettebondarek@gmail.com>
2020-11-06 20:10:14 -0500

    added space for shcedule, and save to local storage, as well as Clear button in jumbotron

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// // identify hours of work 9AM-5PM
-// // create variables 
-// var workHours = []; 
-// var availableHours = {};
-// // moment().year(year).month(month).date(day)
-// var day = moment();
-// var currentTime = m.hour();
-
-// // Need clock to track current day time
-// function clock() { 
-//     var dateString = moment().format('MMMM Do YYYY, h:mm:ss a');
-//      $('#currentDay').html(dateString);
-// }
-
-// // clock times and areas to schedule tasks
-
-// for (var hour = 9; hour < 18;) {
-//     workHours.push(moment({hour}).format());
-//     // need to append
-//     $('.container').append('<div class='row time-block' data-time='${hour}'>
-//     <div class= 'col-md hour'>
-//         <p>${moment({hour}).format('h a')}</p>
-//     );
-// }
-
-// // 1 hour increments for scheduling on hour
-// $('time-block').each(function() {
-// $(this).find('.text-area').val(availableHours[$(this).attr('date-time')].value);
-// });
-// // how to save to local storage
 $(document).ready(function(){
 
 $(".saveBtn").on("click", function(){
@@ -38,5 +8,20 @@ $(".saveBtn").on("click", function(){
     console.log(localStorage)
 
     localStorage.setItem(hour, activity)
-    
-})
+    localStorage.getItem(hour, activity)
+
+});
+
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-1 .description").val(localStorage.getItem("hour-1"))
+$("#hour-2 .description").val(localStorage.getItem("hour-2"))
+$("#hour-3 .description").val(localStorage.getItem("hour-3"))
+$("#hour-4 .description").val(localStorage.getItem("hour-4"))
+$("#hour-5 .description").val(localStorage.getItem("hour-5"))
+
+document.getElementById('hour').value = inputValue;
+});
